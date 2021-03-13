f88bddc1041e31b29d96e11d35b71ee6a2928b4b
Thales Bruno <thalesbrunom@gmail.com>
2021-03-13 07:35:52 -0300

    Add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import React, { Component } from 'react'
+import Navbar from './components/layout/Navbar'
+import './App.css'
 
-function App() {
-    return (
-        <div className="App">
-            <h1>Hello from React</h1>
-        </div>
-    );
+class App extends Component {
+
+    render() {
+
+        return (
+            <Navbar />
+        );
+    }
 }
 
 export default App;
