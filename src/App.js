7dc0df475b710d178a9b645841226acf347cb368
Thales Bruno <thalesbrunom@gmail.com>
2021-03-14 06:56:32 -0300

    Add Search component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Navbar from './components/layout/Navbar'
 import Users from './components/users/Users'
 import Spinner from './components/layout/Spinner'
+import Search from './components/users/Search'
 import './App.css'
 import axios from 'axios'
 
@@ -17,12 +18,19 @@ class App extends Component {
         this.setState({ users: res.data, loading: false })
     }
 
+    searchUsers = async text => {
+        this.setState({ loading: true })
+        const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+        this.setState({ users: res.data.items, loading: false })
+    }
+
     render() {
 
         return (
             <div className='App'>
                 <Navbar />
                 <div className="container">
+                    <Search searchUsers={this.searchUsers} />
                     {this.state.loading ? <Spinner /> : <Users users={this.state.users} />}
                 </div>
             </div>
